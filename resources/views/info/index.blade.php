36da4765cbf5414c6113de3f42f35cea6a9e9674
wumpfroot <itswumpa@gmail.com>
2025-01-09 18:59:46 +0200

    Added game pages with dynamic routing

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -6,11 +6,27 @@
 </head>
 <body>
     <h1>Info section</h1>
+    <p>{{ $greeting }}</p>
 
     <h2>Games I am playing right now</h2>
 
     <ul>
-        <li>Games are here:</li>
+        <h3>Games are as follows:</h3>
+        <li>
+            <a href="/info/{{$games[0]["id"]}}">
+                {{ $games[0]["name"] }}
+            </a>
+        </li>
+        <li>
+            <a href="/info/{{$games[1]["id"]}}">
+                {{ $games[1]["name"] }}
+            </a>
+        </li>
+        <li>
+            <a href="/info/{{$games[2]["id"]}}">
+                {{ $games[2]["name"] }}
+            </a>
+        </li>
     </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,15 @@ Route::get('/', function () {
 });
 
 Route::get('/info', function () {
-    return view('info.index');
+    $games = [
+        ["name" => "Path of Exile", "studio" => "Grinding Gear Games", "id" => "1"],
+        ["name" => "Marvel Rivals", "studio" => "NetEase Games", "id" => "2"],
+        ["name" => "Balatro", "studio" => "LocalThunk", "id" => "3"]
+    ];
+    return view('info.index', ["greeting" => "sup, how you doing", "games" => $games]);
+});
+
+Route::get('/info/{id}', function ($id) {
+    // fetch game with id
+    return view('info.show', ["id" => $id]);
 });
